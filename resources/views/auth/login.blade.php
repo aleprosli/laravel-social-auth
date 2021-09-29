1787bd57fad695566b86601fc5ab409550c99d85
Aliff <aliff.rosli96@gmail.com>
2021-09-29 23:22:30 +0800

    add button redirect to url facebook

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,6 +57,10 @@
                                     {{ __('Login') }}
                                 </button>
 
+                                <a class="ml-1 btn btn-primary" href="{{ url('auth/facebook') }}" style="margin-top: 0px !important;background: blue;color: #ffffff;padding: 5px;border-radius:7px;" id="btn-fblogin">
+                                    <i class="fa fa-facebook-square" aria-hidden="true"></i> Login with Facebook
+                                </a>
+
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
