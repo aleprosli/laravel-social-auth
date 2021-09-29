5fab7d4acba829f8a9edc9f62be1e940f485ed59
Aliff <aliff.rosli96@gmail.com>
2021-09-29 23:22:13 +0800

    route add

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('auth/facebook', [App\Http\Controllers\FacebookController::class, 'redirectToFacebook']);
+Route::get('auth/facebook/callback', [App\Http\Controllers\FacebookController::class, 'handleFacebookCallback']);
 
 Route::get('/', function () {
     return view('welcome');
