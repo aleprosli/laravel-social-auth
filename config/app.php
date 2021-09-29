4af7d6da9a9774d19176447243153ddb1f3c9eac
Aliff <aliff.rosli96@gmail.com>
2021-09-30 00:26:25 +0800

    add package app

--- config/app.php
+++ config/app.php
@@ -166,6 +166,9 @@ return [
          * Package Service Providers...
          */
 
+        Laravel\Socialite\SocialiteServiceProvider::class,
+
+
         /*
          * Application Service Providers...
          */
@@ -228,6 +231,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+
 
     ],
 
